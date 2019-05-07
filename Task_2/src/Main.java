c4acb9ad0c21ff24a589563544ed84dcc9183bf3
Nazariy <tymtsiv2710@gmail.com>
2019-05-08 00:28:08 +0300

    Add Task_2

---
+++ Task_2/src/Main.java
@@ -0,0 +1,12 @@
+package  src;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        javax.swing.JFrame frame = new view.MainFrame();
+
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
